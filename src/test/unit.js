35169985594f67fe00dea1075a410edb08f01bb2
MisterTakaashi <soapmctravich@gmail.com>
2020-04-07 12:27:51 +0200

    Added test for false lean option in books

--- src/test/unit.js
+++ src/test/unit.js
@@ -2,6 +2,7 @@ const sinon = require('sinon');
 const chai = require('chai');
 const mongoose = require('mongoose');
 const expect = chai.expect;
+const should = require('chai').should();
 
 const booksRepo = require('../repositories/books');
 
@@ -12,15 +13,29 @@ before(() => {
 });
 
 describe('Books list', () => {
-  it('should return all books', async () => {
+  it('should return two books with lean option', async () => {
     const response = await booksRepo.findAllBooks(true);
 
     expect(response).to.be.deep.equal(filmListMock);
   });
 
+  it('should return two books without lean option', async () => {
+    const response = await booksRepo.findAllBooks();
+
+    expect(response).to.have.length(2);
+    expect(response[0]).to.have.property("_id");
+    expect(response[1]).to.have.property("_id");
+  });
+});
+
+describe('Book', () => {
   it('should return metro book', async () => {
-    const response = await booksRepo.findBookById("8365315017", true);
+    const response = await booksRepo.findBookById('8365315017', true);
 
     expect(response).to.be.deep.equal(filmListMock[0]);
   });
-});
+
+  it('should throw a BookNotFoundError', (done) => {
+    expect(booksRepo.findBookById('10')).to.be.rejectedWith(Error, 'Cannot find book 10').notify(done);
+  });
+})
